dc433d07b00cf028fe94657aef30113f664958fc
Rommel Rico <me@rommelrico.com>
2018-09-07 20:09:34 -0700

    Project comments

--- src/WordCount.kt
+++ src/WordCount.kt
@@ -1,3 +1,6 @@
 fun main(args: Array<String>) {
-    
+    val allTheWords = "hello there my name is rommel"
+    // TODO: Get a counted list of all the words.
+    // TODO: Sort the list.
+    // TODO: Print a sorted list of most popular words.
 }
