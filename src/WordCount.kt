51130d97dd2f154c1e9f1f41e2ee448c3d1bcc93
Rommel Rico <me@rommelrico.com>
2018-09-08 22:21:53 -0700

    Regular expressions in Kotlin

--- src/WordCount.kt
+++ src/WordCount.kt
@@ -1,7 +1,11 @@
 fun main(args: Array<String>) {
     // TODO: Read from a file and turn into a string. 
     val allTheWords = "hello there my name is rommel"
-    // TODO: Make a list of all the words separated out.
+
+    // Make a list of all the words separated out.
+    val words = allTheWords.split(" ")
+    println(words.size)
+
     // TODO: Get a counted list of all the words.
     // TODO: Sort the list.
     // TODO: Print a sorted list of most popular words.
